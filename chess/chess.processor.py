7d8f071a8281153d211c8aabb72d410248b54944
David Poirier-Quinot <davipoir@gmail.com>
2015-01-31 09:46:27 +0100

    linked animation in mountain.blend, added record file in bench to avoid error, removed import PyQt4 from chess.processor.py is_console() mode

--- chess/chess.processor.py
+++ chess/chess.processor.py
@@ -1,23 +1,23 @@
 ## Copyright (C) LIMSI-CNRS (2014)
 ##
 ## contributor(s) : Jorge Gascon, Damien Touraine, David Poirier-Quinot,
-## Laurent Pointal, Julian Adenauer, 
-## 
+## Laurent Pointal, Julian Adenauer,
+##
 ## This software is a computer program whose purpose is to distribute
 ## blender to render on Virtual Reality device systems.
-## 
+##
 ## This software is governed by the CeCILL  license under French law and
-## abiding by the rules of distribution of free software.  You can  use, 
+## abiding by the rules of distribution of free software.  You can  use,
 ## modify and/ or redistribute the software under the terms of the CeCILL
 ## license as circulated by CEA, CNRS and INRIA at the following URL
-## "http://www.cecill.info". 
-## 
+## "http://www.cecill.info".
+##
 ## As a counterpart to the access to the source code and  rights to copy,
 ## modify and redistribute granted by the license, users are provided only
 ## with a limited warranty  and the software's author,  the holder of the
 ## economic rights,  and the successive licensors  have only  limited
-## liability. 
-## 
+## liability.
+##
 ## In this respect, the user's attention is drawn to the risks associated
 ## with loading,  using,  modifying and/or developing or reproducing the
 ## software by the user in light of its specific status of free software,
@@ -25,13 +25,13 @@
 ## therefore means  that it is reserved for developers  and  experienced
 ## professionals having in-depth computer knowledge. Users are therefore
 ## encouraged to load and test the software's suitability as regards their
-## requirements in conditions enabling the security of their systems and/or 
-## data to be ensured and,  more generally, to use and operate it in the 
-## same conditions as regards security. 
-## 
+## requirements in conditions enabling the security of their systems and/or
+## data to be ensured and,  more generally, to use and operate it in the
+## same conditions as regards security.
+##
 ## The fact that you are presently reading this means that you have had
 ## knowledge of the CeCILL license and that you accept its terms.
-## 
+##
 
 import blendervr
 import os
@@ -54,7 +54,7 @@ if blendervr.is_virtual_environment():
             #self.logger.debug('objets : ', self._scene.objects)
 
             # Don't move the floor and the edges ...
-            self._laser.allowDisallowObjects(True) 
+            self._laser.allowDisallowObjects(True)
             self._laser.allowDisallowObjects(False, self._scene.objects['Ground'])
             self._laser.allowDisallowObjects(False, self._scene.objects['Edges'].children)
 
@@ -164,9 +164,9 @@ if blendervr.is_virtual_environment():
                     self.reset()
                     return
             except KeyError:
-                pass 
+                pass
             super(Processor, self).keyboardAndMouse(info)
-           
+
 
         def send_volume(self):
             for id, obj in self._sound_objects.items():
@@ -189,7 +189,7 @@ if blendervr.is_virtual_environment():
 elif blendervr.is_creating_loader():
 
     import bpy
-    
+
     class Processor(blendervr.processor.getProcessor()):
 
         def __init__(self, creator):
@@ -197,8 +197,8 @@ elif blendervr.is_creating_loader():
 
 elif blendervr.is_console():
 
-    from PyQt4 import QtCore, QtGui, uic
-    
+    # from PyQt4 import QtCore, QtGui, uic
+
     class Processor(blendervr.processor.getProcessor()):
 
         def __init__(self, console):
