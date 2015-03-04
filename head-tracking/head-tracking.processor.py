01e8a5a22d93c59715e27e1c8cf2ec3678ce5b66
Dalai Felinto <dfelinto@gmail.com>
2015-03-04 12:41:05 -0300

    Head-Traking: add calibration, start, reset and quit commands (1-4)

--- head-tracking/head-tracking.processor.py
+++ head-tracking/head-tracking.processor.py
@@ -32,21 +32,70 @@ if blendervr.is_virtual_environment():
             try:
                 from blendervr.player import device
 
-                if info['key'] == ord('q'):
-                    self.blenderVR.quit("pressed 'q' key")
+                if info['state'] == device.STATE_PRESS:
+                    if info['key'] == ord('q'):
+                        self.blenderVR.quit("pressed 'q' key")
 
-                elif (info['key'] == ord('v')
-                        and info['state'] == device.STATE_PRESS):
-                    self._viewpoint.activation(not self._viewpoint.isActivated())
-                    return
+                    elif info['key'] == ord('v'):
+                        self._viewpoint.activation(not self._viewpoint.isActivated())
+
+                    # Head Navigation Setup
+                    elif info['key'] == ord('1'):
+                        self.logger.info("Calibrating Navigation")
+                        for user in info['users']:
+                            self._navigator.update(self._navigator.CALIBRATE, user)
+
+                    elif info['key'] == ord('2'):
+                        self.logger.info("Start Navigation")
+                        for user in info['users']:
+                            self._navigator.update(self._navigator.TOGGLE, user)
+
+                    elif info['key'] == ord('3'):
+                        self.logger.info("Reset Navigation")
+                        self.reset(info['users'])
+
+                    elif info['key'] == ord('4'):
+                        self.logger.info("Quitting")
+                        self.blenderVR.quit("Because user asked!")
 
             except (KeyError, SystemExit):
                 pass
 
-            except:
-                self.logger.log_traceback(False)
+            except Exception as err:
+                self.logger.log_traceback(err)
 
             super(Processor, self).keyboardAndMouse(info)
+
+        def reset(self, users=None):
+            """
+            Reset all the user position and orientations.
+            A new calibration will be required.
+            """
+            if not hasattr(self, '_navigator'):
+                return
+
+            if users is None:
+                users = list(self.blenderVR.getAllUsers().values())
+
+            for user in users:
+                self._navigator.update(self._navigator.RESET, user)
+                user.resetVehiclePosition()
+
+        def user_position(self, info):
+            """
+            Callback for one of the sensors of a tracker device.
+            Defined in the XML config file.
+
+            Callback defined in the XML config file to one of the VRPN tracker sensors
+            """
+
+            try:
+                for user in info['users']:
+                    self._navigator.setHeadLocation(user, info)
+            except Exception as err:
+                self.logger.log_traceback(err)
+
+            super(Processor, self).user_position(info)
 
         def user_position(self, info):
             """
