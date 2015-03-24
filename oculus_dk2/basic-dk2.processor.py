c9b106423d40f37fc606394eb314c8335297df1c
David Poirier-Quinot <davipoir@gmail.com>
2015-03-24 15:43:45 +0100

    renamed basic-dk2 scene, cleaned associated processor.py

--- oculus_dk2/basic-dk2.processor.py
+++ oculus_dk2/basic-dk2.processor.py
@@ -10,6 +10,20 @@ if blendervr.is_virtual_environment():
             if self.blenderVR.isMaster():
                 self.blenderVR.getSceneSynchronizer().getItem(bge.logic).activate(True, True)
 
+        def user_position(self, info):
+            """
+            Callback defined in the XML config file to one of the Oculus DK2
+
+            This function is optional but it's useful for debugging or non-standard
+            usage of the Oculus DK2 devices.
+
+            An analog function defined in the main Processor already takes care of
+            updating the position (and orientation) of the users, and it's used
+            as a callback when no processor_method is specified in the XML config file
+            """
+            # self.logger.debug('RIGHT', info['users'][0].getName(), len(info['users']))
+            super(Processor, self).user_position(info)
+
 
 elif blendervr.is_creating_loader():
     import bpy
