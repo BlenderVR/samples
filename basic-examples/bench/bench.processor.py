40ed41a19a5c74b94f2c7c455a1833d4503fbc7a
Dalai Felinto <dfelinto@gmail.com>
2015-04-08 15:47:45 -0300

    Final "Automatic rename of blenderVR to BlenderVR" by David + removal of auto-generated designer/*.py

--- basic-examples/bench/bench.processor.py
+++ basic-examples/bench/bench.processor.py
@@ -1,23 +1,23 @@
 ## Copyright (C) LIMSI-CNRS (2014)
 ##
 ## contributor(s) : Jorge Gascon, Damien Touraine, David Poirier-Quinot,
-## Laurent Pointal, Julian Adenauer, 
-## 
+## Laurent Pointal, Julian Adenauer,
+##
 ## This software is a computer program whose purpose is to distribute
 ## blender to render on Virtual Reality device systems.
-## 
+##
 ## This software is governed by the CeCILL  license under French law and
-## abiding by the rules of distribution of free software.  You can  use, 
+## abiding by the rules of distribution of free software.  You can  use,
 ## modify and/ or redistribute the software under the terms of the CeCILL
 ## license as circulated by CEA, CNRS and INRIA at the following URL
-## "http://www.cecill.info". 
-## 
+## "http://www.cecill.info".
+##
 ## As a counterpart to the access to the source code and  rights to copy,
 ## modify and redistribute granted by the license, users are provided only
 ## with a limited warranty  and the software's author,  the holder of the
 ## economic rights,  and the successive licensors  have only  limited
-## liability. 
-## 
+## liability.
+##
 ## In this respect, the user's attention is drawn to the risks associated
 ## with loading,  using,  modifying and/or developing or reproducing the
 ## software by the user in light of its specific status of free software,
@@ -25,13 +25,13 @@
 ## therefore means  that it is reserved for developers  and  experienced
 ## professionals having in-depth computer knowledge. Users are therefore
 ## encouraged to load and test the software's suitability as regards their
-## requirements in conditions enabling the security of their systems and/or 
-## data to be ensured and,  more generally, to use and operate it in the 
-## same conditions as regards security. 
-## 
+## requirements in conditions enabling the security of their systems and/or
+## data to be ensured and,  more generally, to use and operate it in the
+## same conditions as regards security.
+##
 ## The fact that you are presently reading this means that you have had
 ## knowledge of the CeCILL license and that you accept its terms.
-## 
+##
 
 import blendervr
 
@@ -58,7 +58,7 @@ if blendervr.is_virtual_environment():
 elif blendervr.is_creating_loader():
 
     import bpy
-    
+
     class Processor(blendervr.processor.getProcessor()):
 
         def __init__(self, creator):
@@ -74,7 +74,7 @@ elif blendervr.is_console():
             super(Common, self).__init__(parent)
 
             self._window = QtGui.QDialog()
-            self._ui = blendervr.tools.gui.load(os.path.join(blendervr.tools.getModulePath(), 'designer', 'scene.ui'), self._window)
+            self._ui = blendervr.tools.gui.load(os.path.join(blendervr.tools.getModulePath(), 'designer', 'bench.ui'), self._window)
 
             self._ui.set_result_filename_postfix.clicked.connect(self.cb_set_result_filename_postfix)
 
